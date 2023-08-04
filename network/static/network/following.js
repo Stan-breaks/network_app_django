6c6ce14638e3ee4a5e1c4a0873171a24a8d87b54
Stan-breaks <108292766+Stan-breaks@users.noreply.github.com>
2023-08-05 02:30:01 +0300

    almost finished

--- network/static/network/following.js
+++ network/static/network/following.js
@@ -1,20 +1,4 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    var user=document.querySelector('h2').textContent
-    document.querySelector('#follow').addEventListener('click',()=>{
-        fetch(`follow/${user}`,{
-            method:'POST',
-            body:JSON.stringify({
-                follow:true
-            })
-        })
-        .then(response=>response.json())
-        .then(result=>{
-            console.log(result)
-        })
-        setTimeout(function() {
-            location.reload();
-        }, 500);
-    })
+document.addEventListener('click',()=>{
     function like(url){
         fetch(url,{
             method:'POST',
